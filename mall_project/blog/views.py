b0f88d7307611bcfb16d20ebd02832fe6e418218
Hyuksoo Shin <shinhyuksoo@Hyuksoos-iMac.local>
2025-01-05 08:49:49 +0900

    add pagination

--- mall_project/blog/views.py
+++ mall_project/blog/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
-
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts' : posts})
+# from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_detail(request, id):
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # paginator : set to 3 posts per pages
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+        
+
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts' : posts})
+
+
+def post_detail(request, year, month, day, post):
 
     # try:
     #     post = Post.published.get(id=id)
@@ -17,8 +41,12 @@ def post_detail(request, id):
     #     raise Http404("No Post found.")
     
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                            #  id=id,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
